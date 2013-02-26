0bad251b7a236798f8cb56e8801ab2dc2151322d
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2013-02-26 23:50:43 +0400

    Initial

---
+++ build.sh
@@ -0,0 +1,463 @@
+#!/bin/sh
+set -e
+
+DIR="$(cd "$(dirname "$0")" && pwd)"
+BUILD_SCRIPT_PATH="$DIR/$(basename "$0")"
+
+# Load ~/.build.config script if exists.
+if [ -f "$HOME/.build.config" ]; then . "$HOME/.build.config"; fi
+
+# Load build.config script if exists.
+if [ -f "$DIR/build.config" ]; then . "$DIR/build.config"; fi
+
+################### Mandatory variables to be defined ##########################
+_checkvar () { if eval "[ -z \"\$$1\" ]"; then echo "Variable $1 is not set"; exit 1; fi }
+
+# Package name.
+_checkvar PKG_NAME
+
+# PPA url. Example: PPA_URL='http://ppa.launchpad.net/myppa'.
+_checkvar PPA_URL
+
+# PPA name for upload.
+_checkvar PPA
+################################################################################
+
+################## Mandatory functions to be implemented #######################
+_checkfunc () { if ! type "$1" 2>&1 >/dev/null; then echo "Function $1 is not implemented" 1>&2; exit 1; fi; }
+_checkfuncs() {
+    # Print version of the package to be built.
+    _checkfunc version
+
+    # Update sources.
+    _checkfunc update
+
+    # Print changelog for the specified distrib. This in non-public function.
+    _checkfunc _changelog
+
+    # Checkout sources to the specified directory. This in non-public function.
+    _checkfunc _checkout
+}
+################################################################################
+
+########################## Optional variables ##################################
+# Package maintainer
+: ${MAINTAINER:='Unknown <unknown@unknown>'}
+
+# Source directories
+: ${SOURCES_DIR:="$DIR/sources"}
+: ${SRC_DIR_NAME:="$PKG_NAME"}
+: ${SRC_DIR:="$SOURCES_DIR/$SRC_DIR_NAME"}
+
+# Path to the debian directory
+: ${DEB_DIR:="$DIR/debian"}
+
+# Source revision
+: ${REV:='origin/master'}
+
+# Cache directories
+: ${CACHE_DIR:="$DIR/cache"}
+: ${BASETGZ_DIR:="$CACHE_DIR/base"}
+: ${APT_CACHE_DIR:="$CACHE_DIR/apt"}
+
+# Temporary build directory
+: ${BUILD_DIR:="$DIR/build"}
+
+# Output directory for built packages
+BUILD_DATE=$(date +%d.%m.%y)
+: ${DISTRIBS_DIR:="$DIR/distribs"}
+: ${DISTRIBS_SRC_DIR:="$DISTRIBS_DIR/$BUILD_DATE/src"}
+: ${DISTRIBS_DEB_DIR:="$DISTRIBS_DIR/$BUILD_DATE/deb"}
+
+# pbuilder arguments
+: ${PBUILDER_ARGS:=--aptcache "$APT_CACHE_DIR" \
+                    --buildplace "$BUILD_DIR" --buildresult "$DISTRIBS_DEB_DIR"\
+                    --override-config}
+[ -z "$http_proxy" ] || PBUILDER_ARGS="$PBUILDER_ARGS --http-proxy "$http_proxy""
+
+# URL of a Debian mirror
+: ${DEB_MIRROR:='http://archive.ubuntu.com/ubuntu'}
+
+# dpkg-buildpackage args
+: ${BUILDPACKAGE_ARGS:="-uc -us"}
+
+# Build script dependencies
+DEPENDS="pbuilder debootstrap lsb-release dpkg-dev debhelper $DEPENDS"
+
+# Aptitude tag to mark all installed dependencies
+: ${DEPENDS_TAG:="$PKG_NAME-build"}
+
+# URL of a ppa containing build dependencies
+: ${PPA_DEPENDS:="deb $PPA_URL/$PPA/ubuntu #DISTRIB# main"}
+
+# URL of ppa sources
+: ${PPA_SOURCES:="$PPA_URL/$PPA/ubuntu/dists/#DISTRIB#/main/source/Sources.gz"}
+
+# PPA version
+: ${PPAN:="ppa1"}
+
+# Target platforms
+: ${TARGET_PLATFORMS:="$(lsb_release -cs):$(dpkg-architecture -qDEB_BUILD_ARCH)"}
+
+# Command aliases
+: ${SUDO:='sudo'}
+: ${RM:='/bin/rm'}
+
+# Skip targets
+: ${SKIP_DEPENDS:='false'}
+: ${SKIP_UPDATE:='false'}
+: ${SKIP_UPDATE_BASE:='false'}
+: ${SKIP_BUILD:='false'}
+: ${SKIP_UPLOAD:='true'}
+################################################################################
+
+################################## Targets #####################################
+
+create() {
+    local version=$(version)
+    local name_ver="${PKG_NAME}_${version}"
+    local src="$BUILD_DIR/$name_ver"
+    local orig_tar="$BUILD_DIR/$name_ver.orig.tar.bz2"
+    
+    # Checkout
+    "$RM" -rf "$BUILD_DIR"
+    mkdir -p "$BUILD_DIR"
+    _checkout "$src"
+    
+    # Create orig source tarball
+    _orig_tarball "$src" "$orig_tar"
+    
+    # Create source packages
+    for dist in $(for i in $TARGET_PLATFORMS; do echo $i | awk -F ':' '{print $1}'; done | sort -u)
+    do
+        local deb_dir="$(_deb_dir "$dist")"
+        [ -d "$deb_dir" ] || (echo "Debian directory does not exists: $deb_dir" 1>&2 && exit 1)
+        
+        $RM -rf "$src/debian"
+        cp -r "$deb_dir" "$src"
+        _changelog "$dist" | _gen_changelog "$version" "$dist" > "$src/debian/changelog"
+        sed -i "s/#MAINTAINER#/$MAINTAINER/" "$src/debian/control"
+
+        cd "$src"
+        dpkg-buildpackage -rfakeroot $BUILDPACKAGE_ARGS -S -sa
+    done
+
+    # Move package to $DISTRIBS_SRC_DIR
+    [ -d "$DISTRIBS_SRC_DIR" ] || mkdir -p "$DISTRIBS_SRC_DIR"
+    cd "$BUILD_DIR"
+    PACKAGES=$(ls *.dsc)
+    mv *.tar.* *.dsc *.changes "$DISTRIBS_SRC_DIR"
+    $RM -rf "$BUILD_DIR"
+}
+
+build() {
+    echo "build:"
+    cd "$DISTRIBS_SRC_DIR"
+    : ${PACKAGES:=$(ls *.dsc)}
+    $RM -rf "$BUILD_DIR"
+
+    for i in $TARGET_PLATFORMS
+    do
+        local distrib=$(echo $i | awk -F ':' '{print $1}')
+        local arch=$(echo $i | awk -F ':' '{print $2}')
+
+        _pbuild_create $distrib $arch
+        _pbuild_build  $distrib $arch
+    done
+
+    cd "$DISTRIBS_DEB_DIR"
+    $RM -f *.tar.bz2 *.dsc *.changes
+    $RM -rf "$BUILD_DIR"
+}
+
+depends() {
+cat << EOF
+depends:
+
+Installing dependencies: $DEPENDS
+
+Note: you may uninstall the installed dependencies with the following command:
+sudo aptitude purge '?user-tag($DEPENDS_TAG)'
+
+EOF
+
+    $SUDO apt-get install aptitude
+    $SUDO aptitude install --add-user-tag "$DEPENDS_TAG" $DEPENDS
+}
+
+clean() {
+    echo "clean:"
+    $RM -rf "$DISTRIBS_DIR"
+}
+
+clean_cache() {
+    echo "clean_cache:"
+    $RM -rf "$CACHE_DIR"
+}
+
+clean_depends() {
+    echo "clean_depends:"
+    $SUDO aptitude purge "?user-tag($DEPENDS_TAG)"
+}
+
+clean_sources() {
+    echo "clean_sources:"
+    $RM -rf "$SRC_DIR"
+}
+
+clean_all() {
+    echo "clean_all:"
+    clean
+    clean_cache
+    clean_depends
+    clean_sources
+}
+
+cur_version() {
+    for dist in $(for i in $TARGET_PLATFORMS; do echo $i | awk -F ':' '{print $1}'; done | sort -u)
+    do
+        echo "$dist: $(_cur_version "$dist")"
+    done
+}
+
+check_updates() {
+    echo "check_updates:"
+    local version="$(version)"
+    local names=''
+    echo -n "Checking updates for $PKG_NAME...\t"
+
+    for dist in $(for i in $TARGET_PLATFORMS; do echo $i | awk -F ':' '{print $1}'; done | sort -u)
+    do
+        local cur_version="$(_cur_version "$dist")"
+        
+        if [ "$version" != "$cur_version" ]
+        then
+            names="$names $dist"
+        fi
+    done
+    
+    if [ -z "$names" ]
+    then
+        echo "up to date."
+        return 0
+    else
+        echo "updates are available for:$names"
+        return 1
+    fi
+}
+
+upload() {
+    echo "upload:"
+    
+    cd "$DISTRIBS_SRC_DIR"
+    : ${PACKAGES:=$(ls *.dsc)}
+    local changes="$(for i in $PACKAGES; do echo $i | sed 's/.dsc$/_source.changes/'; done)"
+
+    echo dput "$PPA" $changes
+    dput "$PPA" $changes
+}
+
+all() {
+    echo "build:"
+    [ "$SKIP_DEPENDS" = "true" ] || depends
+    [ "$SKIP_UPDATE" = "true" ] || update
+    create_pkgs
+    [ "$SKIP_BUILD" = "true" ] || build_pkgs
+    [ "$SKIP_UPLOAD" = "true" ] || upload
+}
+################################################################################
+
+############################## Main function ###################################
+_main() {
+    _checkfuncs
+    local TARGETS=$(_print_functions | tr '\n' '|' | head -c -1)
+    
+    if [ -z "$1" ]
+    then 
+        local targets='all'
+    else
+        local targets="$@"
+    fi
+
+    for t in $targets
+    do
+        if echo "$t" | grep -Eq "^($TARGETS)\$"
+        then
+            $t
+        else
+            echo "Unknown target $t"
+            echo "Usage: $(basename "$BUILD_SCRIPT_PATH") <$TARGETS>"
+        fi
+    done
+}
+################################################################################
+
+########################## Non-public functions ################################
+
+_git_update() {
+    local src_url="$1"
+    local src_dir="${2:-"$SRC_DIR"}"
+    
+    if [ ! -d "$src_dir" ]
+    then
+        git clone --no-checkout "$src_url" "$src_dir"
+    else
+        git --git-dir="$src_dir/.git" fetch
+    fi
+}
+
+_git_checkout() {
+    local dest="$1"
+    local rev="$2"
+    local src_dir="${3:-"$SRC_DIR"}"
+    local subdir="$4"
+    
+    
+    if [ -z "$subdir" ]
+    then
+        mkdir -p "$dest"
+        git --git-dir="$src_dir/.git" --work-tree="$dest" reset --hard "$rev"
+    else
+        local tmp="$BUILD_DIR/tmp.co.dir"
+        mkdir -p "$tmp"
+        git --git-dir="$src_dir/.git" --work-tree="$tmp" reset --hard "$rev"
+        mv "$tmp/$subdir" "$dest"
+        "$RM" -rf "$tmp"
+    fi
+}
+
+_git_changelog() {
+    local rev1="$1"
+    local rev2="$2"
+    local src_dir="${3:-"$SRC_DIR"}"
+    local path="$4"
+    local format="${5:-"  * %B%n"}"
+    local rev;
+    rev2="$(git --git-dir="$src_dir/.git" log -n1 --format='%H' "$rev2" -- "$path")"
+    
+    if [ "$rev1" = "Unknown" ]
+    then
+        rev="$rev2"
+    else
+        rev1="$(git --git-dir="$src_dir/.git" log -n1 --format='%H' "$rev1" -- "$path")"
+        rev="$rev1..$rev2"
+    fi
+    
+    if [ -z "$(git --git-dir="$src_dir/.git" log -n1 --format='%H' "$rev" -- "$path")" ]
+    then
+        echo "  * Revision: $rev2\n"
+    else
+        git --git-dir="$src_dir/.git" log --format="$format" "$rev" -- "$path" | \
+        sed -r  '/  \* /! s/^(.*)$/    \1/g'
+    fi
+}
+
+_gen_changelog() {
+    local version="$1"
+    local dist="$2"
+    local qualifier="${3:-"-$PPAN~$dist"}"
+    local date="${4:-"$(date -R)"}"
+    
+    echo "$PKG_NAME (${version}${qualifier}) $dist; urgency=medium"
+    echo
+    cat
+    echo " -- $MAINTAINER  $date"
+}
+
+_orig_tarball() {
+    local src="$1"
+    local dest="$2"
+    local options="${3:-"-cjf"}"
+    tar -C "$(dirname "$src")" $options "$dest" "$(basename "$src")"
+}
+
+_print_functions() {
+    local pattern='^\s*[a-z]\w+\s*\(\s*\)'
+    (grep -Eo "$pattern" "$BUILD_SCRIPT_PATH" | tr -d '[ \t\(\)]'; \
+    grep -E '^\s*\.\s+' "$BUILD_SCRIPT_PATH" | awk '{print $2}' | \
+    while read i; do grep -Eo "$pattern" "$i" | tr -d '[ \t\(\)]'; done) | \
+    sort -u
+}
+
+_gen_pbuilderrc() {
+    local distrib="$1"
+    local pbuilderrc="$2"
+    local ppa_depends="$(echo "$PPA_DEPENDS" | sed "s/#DISTRIB#/$distrib/g")"
+    
+    if [ -f "$DIR/.pbuilderrc" ]
+    then
+    	sed "s/#DISTRIB#/$distrib/g; s;#DEB_MIRROR#;$DEB_MIRROR;g; \
+             s;#PPA_DEPENDS#;$ppa_depends;g" < "$DIR/.pbuilderrc" > "$pbuilderrc"
+    else
+cat << EOF > "$pbuilderrc"
+ALLOWUNTRUSTED=yes
+APTCACHEHARDLINK=no
+BUILDRESULTUID=$SUDO_UID
+OTHERMIRROR="deb $DEB_MIRROR $distrib main restricted universe multiverse|\
+             deb $DEB_MIRROR $distrib-security main restricted universe multiverse|\
+             deb $DEB_MIRROR $distrib-updates main restricted universe multiverse|\
+             $ppa_depends"
+EOF
+    fi
+}
+
+_pbuild_create() {
+    local distrib=$1
+    local arch=$2
+    local btgz="$BASETGZ_DIR/${distrib}_${arch}.tgz"
+    local pbuilderrc="$BUILD_DIR/$distrib.pbuilderrc"
+
+    if [ ! -f "$btgz" ]
+    then
+        echo "Creating base tarball: $btgz"
+        [ -d "$BUILD_DIR" ] || mkdir -p "$BUILD_DIR"
+        [ -d "$BASETGZ_DIR" ] || mkdir -p "$BASETGZ_DIR"
+        [ -d "$APT_CACHE_DIR" ] || mkdir -p "$APT_CACHE_DIR"
+        [ -d "$DISTRIBS_DEB_DIR" ] || mkdir -p "$DISTRIBS_DEB_DIR"
+
+
+        _gen_pbuilderrc "$distrib" "$pbuilderrc"
+        $SUDO pbuilder create --configfile "$pbuilderrc" --debootstrapopts --variant=buildd --basetgz "$btgz"\
+              --distribution ${distrib} --architecture ${arch} \
+              $PBUILDER_ARGS || ($RM -f "$btgz" && return 1)
+    elif [ "$SKIP_UPDATE_BASE" != "true" ]
+    then
+        echo "Updating base tarball: $btgz"
+        [ -d "$BUILD_DIR" ] || mkdir -p "$BUILD_DIR"
+        [ -d "$APT_CACHE_DIR" ] || mkdir -p "$APT_CACHE_DIR"
+
+        _gen_pbuilderrc "$distrib" "$pbuilderrc"
+        $SUDO pbuilder update --configfile "$pbuilderrc" --basetgz "$btgz" $PBUILDER_ARGS
+    fi
+}
+
+_pbuild_build() {
+    local distrib=$1
+    local arch=$2
+    local btgz="$BASETGZ_DIR/${distrib}_${arch}.tgz"
+    local pkgs=$(for i in $PACKAGES; do case $i in *~$distrib.dsc) echo $i;; esac; done)
+    local pbuilderrc="$BUILD_DIR/$distrib.pbuilderrc"
+
+    echo "Building packages: $pkgs"
+    [ -d "$BUILD_DIR" ] || mkdir -p "$BUILD_DIR"
+    [ -d "$DISTRIBS_DEB_DIR" ] || mkdir -p "$DISTRIBS_DEB_DIR"
+    [ -d "$APT_CACHE_DIR" ] || mkdir -p "$APT_CACHE_DIR"
+
+    _gen_pbuilderrc "$distrib" "$pbuilderrc"
+    $SUDO pbuilder build --configfile "$pbuilderrc" --basetgz "$btgz" $PBUILDER_ARGS $pkgs
+}
+
+_cur_version() {
+    local dist="$1"
+    [ ! -z "$dist" ] || (echo "Distrib name is not specified" 1>&2; exit 1)
+    local url="$(echo "$PPA_SOURCES" | sed "s/#DISTRIB#/$dist/")"
+    local version="$(curl "$url" 2>/dev/null | gunzip 2>/dev/null | \
+    grep "^Package: $PKG_NAME$" -A2 | grep '^Version: ' | awk '{print $2}')"
+    ([ -z "$version" ] && echo "Unknown") || echo "${version%-*}"
+}
+
+# Print (and possibly generate) path to the deb directory for the specified distrib.
+_deb_dir() {
+    echo "$DEB_DIR"
+}
+################################################################################
